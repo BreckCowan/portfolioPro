c13162937c4e7d4b83b130411a1a15bfc25bc4d4
Breck Cowan <breckcowan@gmail.com>
2022-02-02 12:18:20 -0600

    got rid of netlify folder

--- server.js
+++ server.js
@@ -14,11 +14,14 @@ app.use("/", contactRoute);
 
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"));
-    app.get("*", (req,res) =>
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    );
+    app.use(express.static(path.join(__dirname, "build")));
+
+    app.get("/", function (req, res) {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
 }
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`server listening on port 5000`));
