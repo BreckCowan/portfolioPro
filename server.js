e9b29a72aef480089330c7847a59a7777bfa418b
Breck Cowan <breckcowan@gmail.com>
2022-03-01 16:16:56 -0600

    udpate

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config;
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
@@ -12,13 +12,12 @@ app.use(cors());
 
 app.use("/", contactRoute);
 
-
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"));
-    app.get("*", (req,res) =>
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    );
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`server listening on port ${PORT}`));
+app.listen(port, console.log(`server listening on port ${process.env.PORT}`));
